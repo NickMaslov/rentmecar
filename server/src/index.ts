ace42fbec4080e9567c8dc9c396ce02aea3047ad
Nick Maslov <kategorychnost@gmail.com>
2020-02-28 20:52:08 -0500

    init ; node/express/typescript setup; tsconfig, linter config, start and build scripts; testing mock data with express routes;

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { listings } from "./listings";
+
+const app = express();
+
+app.get("/listings", (_req, res) => {
+  res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1)[0]);
+    }
+  }
+
+  return res.send("failed to deleted listing");
+});
+
+const port = 9000;
+
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
