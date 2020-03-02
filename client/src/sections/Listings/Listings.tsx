3f674155b996cda5e0f8036164b18f1f5a92737c
Nick Maslov <kategorychnost@gmail.com>
2020-03-02 05:29:51 -0500

    client: created functional component Listings; created server.fetch() with POST request to conduct GraphQL queries and mutations

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { server } from "../../lib/api";
+import {
+  ListingsData,
+  DeleteListingData,
+  DeleteListingVariables
+} from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data.listings); // listings exists in data
+  };
+
+  const deleteListing =  async () => {
+    const  data  = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "5e5c3803c4b45d7560e66890"// hardcoded id variable,
+      }
+    });
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+      <button onClick={deleteListing}>Delete a listing!</button>
+    </div>
+  );
+};
