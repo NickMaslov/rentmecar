bab6d445a6fdc4cac89d3f7f8144e03c4ebf4ccc
Nick Maslov <kategorychnost@gmail.com>
2020-03-26 23:39:24 -0400

    Brought on ant desigh to UI; created custom css structure; upfated Listings.tsx, created ListingsSkeleton.tsx for loading and error handling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles/index.css";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { Listings } from "./sections";
