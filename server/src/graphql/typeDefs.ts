5960c8a9b16c09a2fc839f4043b35197751358a5
Nick Maslov <kategorychnost@gmail.com>
2020-06-12 13:45:13 -0400

    added ListingArgs to listings to  query with filter options; created UI for landing Home page, with search bar, feathured options, and suggested promotions

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -34,6 +34,11 @@ export const typeDefs = gql`
     numOfGuests: Int!
   }
 
+  enum ListingsFilter {
+    PRICE_LOW_TO_HIGH
+    PRICE_HIGH_TO_LOW
+  }
+
   type Listings {
     total: Int!
     result: [Listing!]!
@@ -66,6 +71,7 @@ export const typeDefs = gql`
     authUrl: String!
     user(id: ID!): User!
     listing(id: ID!): Listing!
+    listings(filter: ListingsFilter!, limit: Int!, page: Int!): Listings!
   }
 
   type Mutation {
