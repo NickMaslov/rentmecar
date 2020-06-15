2b2af809ff2a88716d85dcdf18716f24db34fc22
Nick Maslov <kategorychnost@gmail.com>
2020-06-15 18:56:31 -0400

    Handled Loading, errors; Added pagination from antd to listings seearch

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
-import { Layout, List, Typography } from "antd";
-import { ListingCard } from "../../lib/components";
+import { Affix, Layout, List, Typography } from "antd";
+import { ListingCard, ErrorBanner } from "../../lib/components";
 import { LISTINGS } from "../../lib/graphql/queries";
 import {
   Listings as ListingsData,
   ListingsVariables,
 } from "../../lib/graphql/queries/Listings/__generated__/Listings";
 import { ListingsFilter } from "../../lib/graphql/globalTypes";
+import {
+  ListingsFilters,
+  ListingsPagination,
+  ListingsSkeleton,
+} from "./components";
 
 const { Content } = Layout;
 const { Paragraph, Text, Title } = Typography;
@@ -19,50 +24,94 @@ interface MatchParams {
 }
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
-  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
-    variables: {
-      location: match.params.location,
-      filter: ListingsFilter.PRICE_LOW_TO_HIGH,
-      limit: PAGE_LIMIT,
-      page: 1,
-    },
-  });
+  const [filter, setFilter] = useState(ListingsFilter.PRICE_LOW_TO_HIGH);
+  const [page, setPage] = useState(1);
+
+  const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(
+    LISTINGS,
+    {
+      variables: {
+        location: match.params.location,
+        filter,
+        limit: PAGE_LIMIT,
+        page
+      },
+    }
+  );
+
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
+
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner
+          description={`
+            We either couldn't find anything matching your search or have encountered an error.
+            If you're searching for a unique location, try searching again with more common keywords.
+          `}
+        />
+        <ListingsSkeleton />
+      </Content>
+    );
+  }
 
   const listings = data ? data.listings : null;
   const listingsRegion = listings ? listings.region : null;
 
-  const listingsSectionElement = listings && listings.result.length ? (
-    <List
-      grid={{
-        gutter: 8,
-        xs: 1,
-        sm: 2,
-        lg: 4,
-      }}
-      dataSource={listings.result}
-      renderItem={(listing) => (
-        <List.Item>
-          <ListingCard listing={listing} />
-        </List.Item>
-      )}
-    />
-  ) : (
-    <div>
-      <Paragraph>
-        It appears that no listings have yet been created for{" "}
-        <Text mark>"{listingsRegion}"</Text>
-      </Paragraph>
-      <Paragraph>
-        Be the first person to create a <Link to="/host">listing in this area</Link>!
-      </Paragraph>
-    </div>
-  );
+  const listingsSectionElement =
+    listings && listings.result.length ? (
+      <div>
+        <Affix offsetTop={64}>
+          <ListingsPagination
+            total={listings.total}
+            page={page}
+            limit={PAGE_LIMIT}
+            setPage={setPage}
+          />
+        </Affix>
+        <ListingsFilters filter={filter} setFilter={setFilter} />
+        <List
+          grid={{
+            gutter: 8,
+            xs: 1,
+            sm: 2,
+            lg: 4,
+          }}
+          dataSource={listings.result}
+          renderItem={(listing) => (
+            <List.Item>
+              <ListingCard listing={listing} />
+            </List.Item>
+          )}
+        />
+      </div>
+    ) : (
+      <div>
+        <Paragraph>
+          It appears that no listings have yet been created for{" "}
+          <Text mark>"{listingsRegion}"</Text>
+        </Paragraph>
+        <Paragraph>
+          Be the first person to create a{" "}
+          <Link to="/host">listing in this area</Link>!
+        </Paragraph>
+      </div>
+    );
 
   const listingsRegionElement = listingsRegion ? (
     <Title level={3} className="listings__title">
       Results for "{listingsRegion}"
     </Title>
   ) : null;
+
+  
 
   return (
     <Content className="listings">
